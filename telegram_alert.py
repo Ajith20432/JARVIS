e8e7d71d8a7c3216e97bcbf93622197a29e4b9ba
Ajith20432 <ajirfhdiadfbfd@gmail.com>
2026-09-01 16:33:18 +0400

    Add files via upload

---
+++ telegram_alert.py
@@ -0,0 +1,6 @@
+# telegram_alert.py (கீழே இதை மட்டும் சேர்க்கவும்)
+
+if __name__ == "__main__":
+    print("Testing Telegram Connection...")
+    send_to_telegram("✅ Boss, J.A.R.V.I.S Telegram is successfully connected and ready for action!")
+    print("Test message triggered!")
